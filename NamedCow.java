fb00e524c7d595cd1c2963e28fcf77d05ccf210c
lauren-tran <laurentran184@gmail.com>
2018-10-26 09:48:39 -0700

    deleted repeating code

--- NamedCow.java
+++ NamedCow.java
@@ -1,15 +1,11 @@
 class NamedCow extends Cow
 {
 	private String myName;
-	private String myType;
-	private String mySound;
     public NamedCow(String type, String name, String sound){
     	myType = type;
     	myName = name;
         mySound = sound;
     }
-	public String getSound(){return mySound;}
-    public String getType(){return myType;}
 	public String getName()
 	{
 		return myName;
